dfd869079641c82fe6e50886efdcc66120511f7a
Bozhok-IA <152988814+Bozhok-IA@users.noreply.github.com>
2024-02-02 22:56:32 +0200

    Create structure project

---
+++ src/main/java/pages/CommonActionsWithElement.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonActionsWithElement {
+    protected WebDriver webDriver;
+
+    public CommonActionsWithElement(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+}
---
+++ src/main/java/pages/PageProvider.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageProvider {
+    private WebDriver webDriver;
+
+    public PageProvider(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public LoginPage loginPage() {
+        return new LoginPage(webDriver);
+    }
+}
